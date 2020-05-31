39e4064351d90a46b6b22af62038ada46a573e04
odan <odan3240@gmail.com>
2020-05-31 20:05:22 +0900

    choice port

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,6 @@ async function bootstrap(): Promise<void> {
   const configService = app.get(ConfigService);
   console.log(configService.get("REDIS_PORT"));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
